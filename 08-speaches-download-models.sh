f0eebe38f5e88c7519221ff3bd8ee73a77ef648f
Deepen Dhulla <deepen@deependhulla.com>
2026-09-16 19:13:43 +0530

    update 2026-09-16

--- 08-speaches-download-models.sh
+++ 08-speaches-download-models.sh
@@ -5,6 +5,8 @@ export SPEACHES_BASE_URL="http://localhost:8090"
 uvx speaches-cli registry ls --task automatic-speech-recognition | jq > /tmp/stt-and-tts-nmodel-list-with-lang-support.json
 
 echo "Downloading (1.5GB) Systran/faster-distil-whisper-large-v3" 
-uvx speaches-cli model download Systran/faster-distil-whisper-large-v3
+#uvx speaches-cli model download Systran/faster-distil-whisper-large-v3
+uvx speaches-cli model download Systran/faster-whisper-small
 echo "Downloading (338M) speaches-ai/Kokoro-82M-v1.0-ONNX" 
-uvx speaches-cli model download speaches-ai/Kokoro-82M-v1.0-ONNX
+#uvx speaches-cli model download speaches-ai/Kokoro-82M-v1.0-ONNX
+uvx speaches-cli model download speaches-ai/Kokoro-82M-v1.0-ONNX-int8
